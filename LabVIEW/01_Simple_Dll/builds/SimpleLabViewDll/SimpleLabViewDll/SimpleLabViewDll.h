7b6164f8da4ed66f16120561344f72a1cfe33a0a
John Stratoudakis <john@wallstreetfpga.com>
2015-04-27 22:42:28 -0400

    Adding updated version of LabView dll binaries

--- LabVIEW/01_Simple_Dll/builds/SimpleLabViewDll/SimpleLabViewDll/SimpleLabViewDll.h
+++ LabVIEW/01_Simple_Dll/builds/SimpleLabViewDll/SimpleLabViewDll/SimpleLabViewDll.h
@@ -14,6 +14,15 @@ int32_t __cdecl Get32bitInteger(void);
  * Set32bitInteger
  */
 void __cdecl Set32bitInteger(int32_t inI32);
+/*!
+ * Set32bitIntegerArray
+ */
+void __cdecl Set32bitIntegerArray(int32_t inI32Array[], int32_t len);
+/*!
+ * Get32bitIntegerArray
+ */
+void __cdecl Get32bitIntegerArray(int32_t out32biIntegerArray[], 
+	int32_t *len);
 
 MgErr __cdecl LVDLLStatus(char *errStr, int errStrLen, void *module);
 
