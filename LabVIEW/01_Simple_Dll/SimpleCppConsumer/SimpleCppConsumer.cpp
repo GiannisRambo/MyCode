3f94e91da464fcfd44cd207ff1028e13014c9331
John Stratoudakis <johnstratoudakis@gmail.com>
2015-04-29 17:00:24 -0400

    Adding some changes

--- LabVIEW/01_Simple_Dll/SimpleCppConsumer/SimpleCppConsumer.cpp
+++ LabVIEW/01_Simple_Dll/SimpleCppConsumer/SimpleCppConsumer.cpp
@@ -8,15 +8,22 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	// Test setting and getting a 32-bit integer
 	Set32bitInteger(200);
 
 	int32_t res = Get32bitInteger();
-
 	std::cout << "\nres = " << res;
+
+	// Test getting an error code from LabVIEW and translating the code
+	// into a string description
+	int32_t errorCode = GetError();
+	std::cout << "\nerrorCode = " << errorCode;
 
 	int32_t testArray[5] = {1, 2, 3, 4, 5};
 	Set32bitIntegerArray(testArray, 5);
-
+	LStrHandle errText;
+	NIGetOneErrorCode(errorCode, &errText);
+	
 	int32_t* targetArray = new int32_t[5];
 	int32_t bytesRead = 5;
 	Get32bitIntegerArray(targetArray, &bytesRead);
